0990133f3ecc9caa34d77500b0df2fb61628cfe8
Denisa Dan <denisa.dan@masabi.com>
2020-11-20 16:48:56 +0200

    initial commit add all files

---
+++ README.md
@@ -0,0 +1,3 @@
+- Played with a Refactoring Kata
+- The requirements are taken from: https://kata-log.rocks/bugs-zero-kata
+- 
---
+++ src/test/java/adaptionsoft/game/trivia/SomeTest.java
@@ -0,0 +1,14 @@
+package adaptionsoft.game.trivia;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeTest {
+
+    @Test
+    public void true_is_true() throws Exception {
+        assertTrue(false);
+    }
+}
